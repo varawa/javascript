ad4348f32a1faad5732526959838343399a8080e
varawa <142249631+varawa@users.noreply.github.com>
2024-05-26 23:29:28 +0530

    Data Types

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 console.log("here")
+//Variables,Constants and Comments .
+
+var score = 34 ;    //Semi colon is optional here .
+var score = 55 ;    //It can be re-declared and re-assigned .
+
+console.log(score)
+
+let points = 23 ;   //It can't be re-declared but can be re-assigned .
+console.log(points) 
+
+const value = 5 ;   //It can't be re-declared or re-assigned .
+
+//DATA TYPES
+
+//Numbers 1,2,33,44,22...
+
+//String "here" , "34" , "fifty" ...
+
+//Boolean true,false
+
+//NULL intentional absence of value .
+
+//let score = null ;
+//score =34 ;
+//console.log(score)
+
+//undefined variable declared but not initialized .
+
+//var point ;
+//console.log(point)
+
+//Object is a complex data structure .
+
+//Symbol 
