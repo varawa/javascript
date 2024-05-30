1eca0a0d3c408d8ab92cf7b4df7166927b489ab3
varawa <142249631+varawa@users.noreply.github.com>
2024-05-30 17:49:44 +0530

    Booleans and Comparison Operators

---
+++ boolean.js
@@ -0,0 +1,19 @@
+//Boolean Values and Comparison Operators
+
+let bool = true ;
+let string = "true" ;
+
+console.log(typeof bool , typeof string) ;
+
+//Methods return Boolean values
+
+let email = "xyzgmail" ;
+
+let booleanval = email.includes("x") ;
+console.log(booleanval) ;
+
+//Comparison Operators
+//  == , === , !== , !=
+//  > , >=
+//  < , <=
+
