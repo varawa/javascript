53e2760aa832f116787417a400fe9b1b39472a50
varawa <142249631+varawa@users.noreply.github.com>
2024-05-31 10:10:13 +0530

    Parameters and arguments

--- Functions/parameters.js
+++ Functions/parameters.js
@@ -1,7 +1,17 @@
 //Passing Values
 
-let invite = function(name){
-    console.log("Welcome ! ", ${name} , "You're invited .") ;
+let invite = function(name = "DefaultValue" , time = "at night"){
+    console.log("Welcome ", name , "You're Invited at ", time) ;
 }
 
-invite("j");
+invite("j","morning");
+invite("k");
+let ageCalc = function(birthYear){
+    let age = 2024 - birthYear ;
+    return age ;
+    //console.log("You're currently ", age , "years old .") ;
+}
+
+console.log("You're currently ",ageCalc(1999)) ;
+
+
