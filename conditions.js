d1b190860d17ac64f3b3688ffe1f569b51f5a101
varawa <142249631+varawa@users.noreply.github.com>
2024-05-31 08:26:41 +0530

    conditions

---
+++ conditions.js
@@ -0,0 +1,44 @@
+//if , else , else if , statements
+
+let budget = 2000 ;
+
+if(budget > 2000){
+    console.log("Greater than 2000") ;
+}
+else if(budget === 2000){
+    console.log("Equal to 2000") ;
+}
+else{
+    console.log("Smaller than 2000") ;
+}
+
+console.log("Out of if else statements") ;
+
+//Nested If
+
+let num = 23 ;
+
+if(num > 22){
+    console.log("First condition is true .") ;
+    if(num == 23){
+        console.log("Second condition is true .") ;
+    }
+}
+
+//BREAK and CONTINUE
+
+for(let i = 1 ; i <= 10 ; i++){
+    if(i == 2){
+        continue ;
+    }
+    console.log(i) ;
+}
+
+//BREAK : Terminates a loop .
+
+for(let j = 1 ; j <= 10 ; j++){
+    if(j == 5){
+        break ;
+    }
+    console.log(j) ;
+}
