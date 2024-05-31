3d736a9e90504ba3cac3a1994e87afdfbbb95756
varawa <142249631+varawa@users.noreply.github.com>
2024-05-31 09:35:27 +0530

    Function Basics

---
+++ Functions/basic.js
@@ -0,0 +1,28 @@
+//Functions
+
+//It is simply a piece of code which we can use in our program .
+//many times .
+
+//It is just like a variable holding some piece of code .
+
+//Declaration
+
+function funcName(){
+    console.log("inside function") ;
+}
+
+//Function Call
+
+funcName() ;
+funcName() ;
+funcName() ;
+
+//ANONYMOUS FUNCTIONS
+
+let fun = function(){
+    console.log("Anonymous function") ;
+}
+
+//Calling Function
+
+fun() ;
