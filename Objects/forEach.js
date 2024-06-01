1f93b066859d491ca824d0e875c9a456bef3b166
varawa <142249631+varawa@users.noreply.github.com>
2024-06-01 23:48:13 +0530

    Create forEach.js

---
+++ Objects/forEach.js
@@ -0,0 +1,9 @@
+//For each method of array .
+
+//Its another type of loop which we use to traverse the array .
+
+let dishes = ["one","two","three","four"] ;
+
+//Using for loop
+
+dishes.forEach(function(element))
