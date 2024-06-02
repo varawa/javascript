fe2aecc6e306ea0d2a8253bf40026044c3994082
varawa <142249631+varawa@users.noreply.github.com>
2024-06-02 12:37:12 +0530

    Inbuilt math library .

---
+++ Objects/math.js
@@ -0,0 +1,15 @@
+//Math Object in built object in Javascript .
+
+console.log(Math) ;
+
+let num = 22.33 ;
+console.log(Math.round(num)) ;
+
+console.log(Math.floor(num)) ;
+console.log(Math.ceil(num)) ;
+
+console.log(Math.trunc(num)) ;  //Remove decimal part .
+
+console.log(Math.random()*100) ;
+
+
