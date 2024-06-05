7fd69184c5c0e3f9e578ffe3c14363deeec6cdd3
varawa <142249631+varawa@users.noreply.github.com>
2024-06-05 20:58:52 +0530

    DOM

---
+++ DOM/Basic.js
@@ -0,0 +1,11 @@
+//Javascript DOM (Document Object Model)
+
+//Created by browser as html loads into the browser .
+
+//Browser create object of html called 'document object' .
+
+//In the document object there is model of complete html 
+//in tree structure .
+
+console.log(document) ;
+
