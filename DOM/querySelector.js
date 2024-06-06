4cbd7a1f42f0b578520d12e63914681b8b977cf1
varawa <142249631+varawa@users.noreply.github.com>
2024-06-06 08:50:27 +0530

    Query Selector

---
+++ DOM/querySelector.js
@@ -0,0 +1,13 @@
+//Getting html element using querySelector/All
+
+//querySelector returns "first element" that match css selector .
+
+//To get all matched elements we use querySelectorAll
+
+//let resultedElement = document.querySelectorAll('.new') ;
+
+//let resultedElement = document.querySelector('.new') ;
+
+let resultedElement = document.querySelectorAll('#news') ;
+
+console.log(resultedElement) ;
