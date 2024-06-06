17ac91993cb1713a248691f46a27f47318d9394a
varawa <142249631+varawa@users.noreply.github.com>
2024-06-06 13:25:21 +0530

    Click behaviour

--- DOM/Events.js
+++ DOM/Events.js
@@ -12,7 +12,8 @@ let elements = document.querySelectorAll('li') ;
 console.log(elements) ;
 
 elements.forEach(function(element){
-    element.addEventListener('click' , function(e){
+    element.addEventListener('click' , e =>{
         console.log(e.target) ;
+        e.target.style.textDecoration = "line-through" ;
     })
 })
