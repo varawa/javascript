56cea8ac852b6b7d1c37a28681b1a69ff8b4c2e2
varawa <142249631+varawa@users.noreply.github.com>
2024-06-06 09:06:37 +0530

    getElementsBy

---
+++ DOM/getElementsBy.js
@@ -0,0 +1,20 @@
+//Get elements by tag name .
+//Only works for tags .
+
+let tagName = document.getElementsByTagName('p') ;
+
+console.log(tagName) ;
+
+
+//Get element by class name .
+
+let className = document.getElementsByClassName('new') ;
+
+console.log(className) ;
+
+
+//Get element by class ID .
+
+let IdName = document.getElementById('news') ;
+
+console.log(IdName) ;
