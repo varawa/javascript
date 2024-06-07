549843323546de5207a9268bbf000ba6ea258916
varawa <142249631+varawa@users.noreply.github.com>
2024-06-07 10:29:13 +0530

    Regex

---
+++ DOM/Events/Regex.js
@@ -0,0 +1,32 @@
+//Regular Expression
+
+//It is a pattern of characters used to do pattern matching .
+//Or we can say for "Data Validation" .
+
+//Implementation of password validation .
+
+//Length atleast 8 .
+//Atleast contain one uppercase letter .
+//Atleast contain one lowercase letter .
+//Atleast contain one digit from 0 to 9 .
+
+let form = document.querySelector('.SignUp') ;
+let email = document.querySelector('#email') ;
+let password = document.querySelector('#password') ;
+
+let PasswordPattern = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).{8,}$"
+
+
+console.log(form) ;
+
+form.addEventListener('submit' , (e)=>{
+    e.preventDefault() ;
+    let PasswordVal = form.userPassword.value ;
+    let result = PasswordVal.match(PasswordPattern)
+    if(result == true){
+        console.log("Strong Password!") ;
+    }
+    else{
+        console.log("Weak Password!") ;
+    }
+})
