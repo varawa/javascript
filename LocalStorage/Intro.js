0b629345f1f01daa6877e30793f5e30f21d5cd41
varawa <142249631+varawa@users.noreply.github.com>
2024-06-08 17:51:34 +0530

    Local Storage

---
+++ LocalStorage/Intro.js
@@ -0,0 +1,12 @@
+//Local Storage
+
+//In previous to-do list task when we refresh the browser we lose our current state data .
+
+
+//Store and retrieve data from database .
+
+//Store and retrieve data from local storage .
+
+//For now we focus on local storage .
+
+console.log(window) ;
