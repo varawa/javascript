f0f3e7813918badb3b872d36652a541dc20a47fb
varawa <142249631+varawa@users.noreply.github.com>
2024-06-08 16:52:36 +0530

    Methods on Date and Time

---
+++ DateAndTime/Methods.js
@@ -0,0 +1,28 @@
+//Date and Time (Methods) 
+
+let completeDateAndTime = new Date() ;
+console.log(completeDateAndTime) ; 
+
+//Get Full Year
+
+console.log(completeDateAndTime.getFullYear()) ;
+
+//Get Month
+
+console.log(completeDateAndTime.getMonth()) ;
+
+//Week Day
+
+console.log(completeDateAndTime.getDay()) ;     //0 : SUNDAY
+
+//Month Day
+
+console.log(completeDateAndTime.getDate()) ;
+
+//Get hours
+
+console.log(completeDateAndTime.getHours()) ;
+
+//Get Minutes
+
+console.log(completeDateAndTime.getMinutes()) ;
