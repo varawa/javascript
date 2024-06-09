2b6e34c92d98968ef3e44cd4d7d970c62bdb0a08
varawa <142249631+varawa@users.noreply.github.com>
2024-06-09 12:40:21 +0530

    Chaining of prototypes .

--- OOP/Prototypes.js
+++ OOP/Prototypes.js
@@ -16,6 +16,8 @@ car.prototype.startEngine = function(){
     console.log("This is start engine method .") ;
 }
 
+car.prototype.company = "Honda" ;
+
 console.log(car.prototype) ;
 
 let newcar = new car("Black" , 2002) ;
@@ -23,4 +25,16 @@ let newcar = new car("Black" , 2002) ;
 console.log(newcar) ;
 newcar.startEngine() ;
 
-console.log(instance.__proto__) ;
+console.log(newcar.__proto__.isPrototypeOf(car)) ;
+
+//It is same as 
+console.log(car.prototype.isPrototypeOf(newcar)) ;
+
+//car.prototype is prototype of all objects created through 
+//the car constructor .
+
+let arr = [2,3,4,5,6,7] ;   //[] is same as new array .
+
+console.log(arr.__proto__ , Array.prototype) ;
+console.log(arr.__proto__.__proto__) ;  //Chaining of prototype.
+console.log(arr) ;
