1b1736b0abf0e5f940ea297de8bc73f3fed1170c
varawa <142249631+varawa@users.noreply.github.com>
2024-06-16 22:59:18 +0530

    Installation

---
+++ SlotMachine/Project.js
@@ -0,0 +1,30 @@
+//1. Deposit some money .
+//2. Determine number of lines to bet on .
+//3. Collect a bet amount .
+//4. Spin the slot machine .
+//5. Check if the user won .
+//6. Give the user their winnings .
+//7. Play Again .
+
+/*function deposit(){
+    return 1 ;
+}
+
+const x = deposit() ;*/
+const prompt = require("prompt-sync")() ;
+
+
+const deposit = () =>{
+    while(true){
+        const depositAmount = prompt("Enter a deposit amount : ") ;
+        const numberDepositAmount = parseFloat(depositAmount) ;
+
+        if(isNaN(numberDepositAmount) || numberDepositAmount <= 0){
+            console.log("Invalid deposit amount , Try Again !") ;
+        } else{
+            return numberDepositAmount ;
+        }
+    }
+} ;
+
+deposit() ;
