8f13a90d1b6b6c199a327b38b100e2b280df70f4
varawa <142249631+varawa@users.noreply.github.com>
2024-06-16 23:08:14 +0530

    Prompt-Sync

--- SlotMachine/Project.js
+++ SlotMachine/Project.js
@@ -13,12 +13,11 @@
 const x = deposit() ;*/
 const prompt = require("prompt-sync")() ;
 
-
 const deposit = () =>{
     while(true){
         const depositAmount = prompt("Enter a deposit amount : ") ;
         const numberDepositAmount = parseFloat(depositAmount) ;
-
+        
         if(isNaN(numberDepositAmount) || numberDepositAmount <= 0){
             console.log("Invalid deposit amount , Try Again !") ;
         } else{
@@ -28,3 +27,18 @@ const deposit = () =>{
 } ;
 
 deposit() ;
+
+const getNumberOfLines = () => {
+    while(true){
+        const lines = prompt("Enter the number of lines to bet on (1-3) : ") ;
+        const numberOfLines = parseFloat(lines) ;
+
+        if(isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3){
+            console.log("Invalid number of lines , Try Again !") ;
+        } else{
+            return numberOfLines ;
+        }
+    }
+}
+
+getNumberOfLines() ;
