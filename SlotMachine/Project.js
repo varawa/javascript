d84ea95a0eb9007e328b6a6e975a227018b9103c
varawa <142249631+varawa@users.noreply.github.com>
2024-06-17 11:21:24 +0530

    Check for winnings

--- SlotMachine/Project.js
+++ SlotMachine/Project.js
@@ -78,11 +78,69 @@ const spin = () => {
             symbols.push(symbol) ;
         }
     }
-    const reels = [[] , [] , []] ;
+    const reels = [] ;
+    for(let i = 0 ; i < COLS ; i++){
+        reels.push([]) ;
+        const reelSymbols = [...symbols] ; 
+        for(let j = 0 ; j < ROWS ; j++){
+            const randomIndex =Math.floor(Math.random()*reelSymbols.length) ;
+            const selectedSymbol = reelSymbols[randomIndex] ;
+            reels[i].push(selectedSymbol) ;
+            reelSymbols.splice(randomIndex , 1) ;
+        }
+    }
+    return reels ;
 };
 
-spin() ;
+const transpose = (reels)=>{
+    const rows = [] ;
+
+    for(let i = 0 ; i < ROWS ; i++){
+        rows.push([]) ;
+        for(let j = 0 ; j < COLS ; j++){
+            rows[i].push(reels[j][i]) ; 
+        }
+    }
+
+    return rows ;
+}
+
+const printRows = (rows)=>{
+    for(const row of rows){
+        let rowString = "" ;
+        for(const [i , symbol] of row.entries()){
+            rowString += symbol ;
+            if(i != row.length - 1){
+                rowString += " | " ;
+            }
+        }
+        console.log(rowString) ;
+    }
+}
+
+const getWinnings = (rows , bet , lines)=>{
+    let winnings = 0 ;
+    for(let row = 0 ; row < lines ; row++){
+        const symbols = rows[row] ;
+        let allSame = true ;
+
+        for(const symbol of symbols ){
+            if(symbol != symbol[0]){
+                allSame = false ;
+                break ;
+            }
+        }
+
+        if(allSame){
+            winnings += bet * SYMBOL_VALUES[symbols[0]] ;
+        }
+    }
+}
 
 let balance = deposit() ;
 const numberOfLines = getNumberOfLines() ;
 const bet  = getBet(balance , numberOfLines) ;
+var reels = spin() ;
+const rows = transpose(reels) ;
+printRows(rows) ;
+
